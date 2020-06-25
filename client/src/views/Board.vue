54e55a84dc24ec3560a5bd4c206fcd1e7d32bdd6
unknown <sonymartha4@gmail.com>
2020-06-26 05:41:29 +0700

    layout, sfx

---
+++ client/src/views/Board.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>  
+        <br><h1 class="text-center">BombSweeper</h1><br><br>
+        <MainGrid v-for="(data,idx) in row" :key="idx" :data="data">
+        </MainGrid>
+  </div>
+</template>
+
+<script>
+import MainGrid from '../components/MainGrid';
+export default {
+  name: 'App',
+  components: {
+    MainGrid
+  },
+
+  data(){
+    //
+    return {
+      row: 10
+      // jumlahKebawah: []
+    }
+  },
+};
+</script>
+<style>
+body {
+    background-image: url(../assets/img/cool-background.png);
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+}
+</style>
