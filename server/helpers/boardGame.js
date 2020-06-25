272df6740ac4f29dd20b7d82bf1febcc98e4d55f
Muh Ichsan Kamil <muhichsank@gmail.com>
2020-06-25 21:14:50 +0800

    add function coord bomb

--- server/helpers/boardGame.js
+++ server/helpers/boardGame.js
@@ -1,7 +1,7 @@
-function randomBomb() {
+function randomBomb(num) {
     let bombs = []
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < num; i++) {
         const temp = [];
         let x = Math.floor(Math.random() * 9);
         let y = Math.floor(Math.random() * 9);
