1e531fd760f97a676867b5ad5d2cd6b47a06bfd8
Muh Ichsan Kamil <muhichsank@gmail.com>
2020-06-26 13:29:09 +0800

    add modal

--- client/src/views/Room.vue
+++ client/src/views/Room.vue
@@ -8,45 +8,65 @@
         <ul>
           <li v-for="(player, i) in players" :key="i">{{ player }}</li>
         </ul>
-        <hr>
+
+        <div>
+          <b-button id="show-btn"  @click="showModal">Open Modal</b-button>
+
+          <b-modal ref="my-modal" hide-header hide-footer>
+            <lottie-player src="https://assets6.lottiefiles.com/private_files/lf30_vKCBXa.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop controls autoplay></lottie-player>
+
+          </b-modal>
+        </div>
+        <hr />
       </div>
       <Board :bombs="bombs"></Board>
     </div>
   </div>
 </template>
 <script>
-import { mapState } from 'vuex'
-import Board from './Board.vue'
+import { mapState } from "vuex";
+import Board from "./Board.vue";
 
 export default {
   name: "BombLand",
   components: {
-    Board,
+    Board
   },
   data() {
     return {
-      message: "",
-    }
+      message: ""
+    };
   },
   computed: {
     roomName() {
-      return localStorage.roomName
+      return localStorage.roomName;
     },
     bombs() {
       if (this.$store.state.rooms[this.$route.params.id - 1].bombs) {
-        return this.$store.state.rooms[this.$route.params.id - 1].bombs
+        return this.$store.state.rooms[this.$route.params.id - 1].bombs;
       }
     },
     players() {
       if (this.$store.state.rooms[this.$route.params.id - 1].players) {
-        return this.$store.state.rooms[this.$route.params.id - 1].players
+        return this.$store.state.rooms[this.$route.params.id - 1].players;
       }
     },
-    roomId(){
-      return this.$route.params.id
+    roomId() {
+      return this.$route.params.id;
     }
   },
-  methods :{
+  methods: {
+    showModal() {
+      this.$refs["my-modal"].show();
+    },
+    hideModal() {
+      this.$refs["my-modal"].hide();
+    },
+    toggleModal() {
+      // We pass the ID of the button that we want to return focus to
+      // when the modal has hidden
+      this.$refs["my-modal"].toggle("#toggle-btn");
+    }
   },
   mounted() {
     // this.$store.dispatch("joinRoom")
